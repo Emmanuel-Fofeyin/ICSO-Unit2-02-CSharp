63705a369416ffd45ef96a37dd58763585cb7937
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-03-22 01:54:37 +0000

    added main.exe file

--- main.cs
+++ main.cs
@@ -1,7 +1,21 @@
-using System;
+// Created by: Emmanuel-Fofeyin
+// Created on: monday march 10th 2020
+//
+// This program does basic math
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine(9 + 2);
+        Console.WriteLine("7 - 3 = " + (7 - 3));
+        Console.WriteLine("4 * 2 = " + (4 * 2));
+        Console.WriteLine("4 + 4 ÷ 2 = " + (4 + (4 / 2)));
+        Console.WriteLine("5 + 2³ = " + (5 + Math.Pow(2, 3)));
+
+        Console.WriteLine("\nDone.");
+    }
 }
